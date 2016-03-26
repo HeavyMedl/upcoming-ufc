c9046f515534d6ac02655fd80b076848ca4448e2
Kurt Medley <kurtmedley@kurt-medleys-mac-pro.local>
2016-03-26 15:16:57 -0700

    got url for card details.. need to get an eachSeries going here

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 const request = require('request');
 const chalk = require('chalk');
-request('http://ufc-data-api.ufc.com/api/v1/us/events', (error, response, body) => {
+request('http://ufc-data-api.ufc.com/api/v3/us/events', (error, response, body) => {
 	if (!error && response.statusCode == 200) {
 		let events = eval(body);
-		events = events
-		.filter(event => {
+		events.filter(event => {
 			let date = new Date(event.event_date);
 			return date > new Date()
+		})
+		.sort((event_a,event_b) => {
+			return new Date(event_a.event_date) > new Date(event_b.event_date) ? 1 : 0
 		})
 		.forEach((event,i) => {
 			let raw_date = new Date(event.event_date);
@@ -19,6 +21,9 @@ request('http://ufc-data-api.ufc.com/api/v1/us/events', (error, response, body) 
 			
 			console.log(`${i+1}. ${date_string} - ${status} ${title} ${location}`);
 			console.log(event.short_description ? event.short_description + '\n' : '');
+
+			// do another call to get card info here. Gonna need to eachSeries
+			//console.log('http://ufc-data-api.ufc.com/api/v3/us/events/'+event.id+'/fights');
 		})
 	}
 });
